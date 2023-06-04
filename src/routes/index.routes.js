261709ff29d777ebcddf701fde4cf9eace86938d
joseedivandro <jose.edivandro.ja@outlook.com>
2023-06-04 17:49:59 -0300

    método post para preencher a tabela likes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,12 +4,14 @@ import hashtag from "./hashtag.routes.js";
 import signRouter from "./auth.routes.js"
 import chalk from "chalk";
 import userRouter from "./user.routes.js";
+import postLikeRouter from "./posts.routes.js";
+
 
 const router = Router();
 router.use(publishRouter);
 router.use(signRouter);
 router.use(hashtag)
 router.use(userRouter);
-
+router.use(postLikeRouter);
 
 export default router;
