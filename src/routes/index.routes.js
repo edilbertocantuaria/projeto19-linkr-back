cce4affe3ead8ae95fdcc2453cf5e59351669c9b
Bernardo Marquez <marquez.cmbh@gmail.com>
2023-06-04 01:01:53 -0300

    feat: get users

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,6 +3,7 @@ import publishRouter from "./publish.routes.js";
 import hashtag from "./hashtag.routes.js";
 import signRouter from "./auth.routes.js"
 import chalk from "chalk";
+import userRouter from "./user.routes.js";
 
 console.log(chalk.green(`QAAAB`));
 
@@ -10,6 +11,7 @@ const router = Router();
 router.use(publishRouter);
 router.use(signRouter);
 router.use(hashtag)
+router.use(userRouter);
 
 
 export default router;
