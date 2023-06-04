ed727e5c4c235f47811b20dc1dfe2b5e053d91f0
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-06-04 12:11:58 -0300

    final hashtag back

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,9 +2,6 @@ import { Router } from "express";
 import publishRouter from "./publish.routes.js";
 import hashtag from "./hashtag.routes.js";
 import signRouter from "./auth.routes.js"
-import chalk from "chalk";
-
-console.log(chalk.green(`QAAAB`));
 
 const router = Router();
 router.use(publishRouter);
