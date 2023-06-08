27666c29fe2f642ea4d27ab1112688244e4cc046
joseedivandro <jose.edivandro.ja@outlook.com>
2023-06-08 01:41:23 -0300

    correcao para aparecer posts do usuario

--- src/controllers/follow.controllers.js
+++ src/controllers/follow.controllers.js
@@ -1,3 +1,4 @@
+import urlMetadata from "url-metadata";
 import { db } from "../database/database.connection.js";
 
 export async function userFollow(req, res) {
@@ -63,22 +64,71 @@ export async function getFollowersAndUsers(req, res) {
 
 export async function getFollowedPosts(req, res) {
     const { followerId } = req.params;
+    const { page = 1, pageSize = 10 } = req.query;
+    const offset = (page - 1) * pageSize;
   
 
     try {
-        console.log(followerId)
-      const posts = await db.query(
-        `
-        SELECT p.id, p.link, p.article, p."userId"
-        FROM posts p
-        INNER JOIN followers f ON p."userId" = f."followedId"
-        WHERE f."followerId" = $1
-        ORDER BY "createdAt" DESC;
-      `,
-        [followerId]
-      );
-  
-      res.status(200).send(posts.rows);
+        const posts = await db.query(
+            `
+            SELECT id, link, article, "userId", "createdAt"
+            FROM (
+              SELECT p.id, p.link, p.article, p."userId", p."createdAt"
+              FROM posts p
+              INNER JOIN followers f ON p."userId" = f."followedId"
+              WHERE f."followerId" = $1
+            
+              UNION
+            
+              SELECT p.id, p.link, p.article, p."userId", p."createdAt"
+              FROM posts p
+              WHERE p."userId" = $1
+            ) AS combinedPosts
+            ORDER BY "createdAt" DESC
+            LIMIT $2
+            OFFSET $3;
+            `,
+            [followerId, pageSize, offset]
+          );
+
+      const postsList = posts.rows;
+
+      if (postsList.length === 0) {
+          return res.status(200).send([]);
+      }
+
+
+      const processedPosts = await Promise.all(
+        postsList.map(async (post) => {
+            try {
+                const metadata = await urlMetadata(post.link);
+                return {
+                    id: post.id,
+                    link: post.link,
+                    title: metadata.title,
+                    description: metadata.description,
+                    image: metadata.image,
+                    article: post.article,
+                    createdAt: post.createdAt,
+                    userId: post.userId,
+                };
+            } catch (error) {
+                console.error(`Error fetching metadata for post with ID ${post.id}:`, error);
+
+                return {
+                    id: post.id,
+                    link: post.link,
+                    title: null,
+                    description: null,
+                    image: null,
+                    article: post.article,
+                    createdAt: post.createdAt,
+                    userId: post.userId,
+                };
+            }
+        })
+    );
+      res.status(200).send(processedPosts);
     } catch (error) {
       res.status(500).send(error);
     }
